3c585faa40a7cac85db4e94e2ae9b450e8428dce
Obee Wonkee Nobee <erik.blessman+r20@gmail.com>
2024-10-03 20:27:15 -0600

    implemented next, prev, add, reset and no-args

--- DnDateCalendar.js
+++ DnDateCalendar.js
@@ -5,17 +5,29 @@ const DnDateCalendar = (() => {
   const schemaVersion = 0.1;
 
   // #region UTILITY FUNCTIONS
-  const isApiMsg = function (msg) {
-    return msg.type == "api" || !/^!dndate(\b\s|$)/.test(msg.content)
+  const isApiMsg = function ($msg) {
+    if (!$msg) {
+      return false;
+    }
+    if ($msg.type != "api") {
+      return false;
+    }
+    if (!$msg.content) {
+      return false;
+    }
+    if (!/^!dndate(\b\s|$)/.test($msg.content)) {
+      return false;
+    }
+    return true;
   }
 
-  const sendException = function (e, msg) {
+  const sendException = function ($e, $msg) {
     sendChat('TokenMod', `/w gm ` +
       `<div style="border:1px solid black; background-color: #fee; padding: .2em; border-radius:.4em;" >` +
       `<div>There was an error while trying to run your command:</div>` +
-      `<div style="margin: .1em 1em 1em 1em;"><code>${msg.content}</code></div>` +
+      `<div style="margin: .1em 1em 1em 1em;"><code>${$msg.content}</code></div>` +
       `<div style="font-size: .6em; line-height: 1em;margin:.1em .1em .1em 1em; padding: .1em .3em; color: #666666; border: 1px solid #999999; border-radius: .2em; background-color: white;">` +
-      JSON.stringify({ msg: msg, version: version, stack: e.stack }) +
+      JSON.stringify({ msg: $msg, version: version, stack: $e.stack }) +
       `</div>` +
       `</div>`
     );
@@ -26,7 +38,7 @@ const DnDateCalendar = (() => {
       state[scriptName] = {
         schemaVersion: schemaVersion,
         dayOfYear: 1,
-        year: 1974,
+        year: 1999,
         months: [
           { name: 'January', days: 31 },
           { name: 'February', days: 28 },
@@ -47,40 +59,83 @@ const DnDateCalendar = (() => {
   // #endregion UTILITY FUNCTIONS
 
   // #region DATE FUNCTIONS
-  const getDate = function () {
-    let dayOfYear = state[scriptName].dayOfYear;
-    const months = state[scriptName].months;
-    if (dayOfYear < 1) {
-      return `Invalid dayOfYear(${dayOfYear})`;
+  const getDate = function ($dayOfYear, $months, $year) {
+    if ($dayOfYear < 1) {
+      log(`Invalid dayOfYear(${$dayOfYear})`);
+      return null;
     }
-    let dayOfMonth = dayOfYear;
+    let dayOfMonth = $dayOfYear;
     let monthIndex = 0;
-    let tmpYear = state[scriptName].year;
-    while (dayOfMonth > months[monthIndex].days) {
-      dayOfMonth -= months[monthIndex++].days;
-      if (monthIndex >= months.length) {
+    while (dayOfMonth > $months[monthIndex].days) {
+      dayOfMonth -= $months[monthIndex++].days;
+      if (monthIndex >= $months.length) {
         monthIndex = 0;
-        ++tmpYear;
+        ++$year;
       }
     }
     return {
       day: dayOfMonth,
-      month: months[monthIndex],
-      year: tmpYear
+      month: $months[monthIndex],
+      year: $year
     };
   }
 
   const getDateStr = function () {
-    const date = getDate();
+    const date = getDate(state[scriptName].dayOfYear, state[scriptName].months, state[scriptName].year);
     return `${date.day} ${date.month.name} ${date.year}`;
+  }
+
+  const addDay = function ($days) {
+    if (isNaN($days)) {
+      sendChat('DnDateCalendar', `/w gm invalid number of days [${$days}]`);
+      return;
+    }
+    const days = parseInt($days);
+    let newDayOfYear = state[scriptName].dayOfYear + days;
+    let newYear = state[scriptName].year;
+    const daysInYear = state[scriptName].months.reduce((acc, month) => acc + month.days, 0);
+
+    if (newDayOfYear > daysInYear) {
+      const yearsToAdd = Math.floor(newDayOfYear / daysInYear);
+      newDayOfYear -= yearsToAdd * daysInYear;
+      newYear += yearsToAdd;
+    } else if (newDayOfYear < 1) {
+      const yearsToRemove = Math.ceil(Math.abs(newDayOfYear) / daysInYear);
+      newDayOfYear += yearsToRemove * daysInYear;
+      newYear -= yearsToRemove;
+    }
+    state[scriptName].dayOfYear = newDayOfYear;
+    state[scriptName].year = newYear;
   }
   // #endregion DATE FUNCTIONS
 
   // #region MESSAGE ROUTING
-  const routeMessage = function (msg) {
-    if (msg.content == '!dndate') {
+  const routeMessage = function ($msg) {
+    if ($msg.content == '!dndate') {
       showCalendar();
+      return;
     }
+    const args = $msg.content.split(' ');
+    args.shift();
+    switch (args[0]) {
+      case 'next':
+        addDay(1);
+        break;
+      case 'prev':
+        addDay(-1);
+        break;
+      case 'add':
+        addDay(args[1]);
+        break;
+      case 'reset':
+        delete state[scriptName];
+        initState();
+        break;
+      default:
+        sendChat('DnDateCalendar', `/w gm unknown command [${$msg.content}]`);
+        break;
+    }
+    showCalendar();
   };
 
   const showCalendar = function () {
@@ -94,14 +149,14 @@ const DnDateCalendar = (() => {
   // #endregion MESSAGE ROUTING
 
   // #region EVENT HANDLERS
-  const onChatMessage = function (msg) {
+  const onChatMessage = function ($msg) {
     try {
-      if (!isApiMsg(msg)) {
+      if (!isApiMsg($msg)) {
         return;
       }
-      routeMessage(msg);
+      routeMessage($msg);
     } catch (e) {
-      sendException(e, msg);
+      sendException(e, $msg);
     }
 
   };
@@ -113,6 +168,7 @@ const DnDateCalendar = (() => {
   on("ready", () => {
     registerEventHandlers();
     initState();
+    log(`==> ${scriptName} v${version} Ready`);
   });
   // #endregion EVENT HANDLERS
 })();
